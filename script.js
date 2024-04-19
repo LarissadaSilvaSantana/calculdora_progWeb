331666555ff004f2c3989236e66528b5bec2528a
Willim-DEV <wpaixao.s2004@gmail.com>
2024-04-18 21:54:50 -0300

    2 parte do codigo e JS feita por William

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 class Calculadora {
 
     constructor() {
@@ -15,10 +16,14 @@ class Calculadora {
             DIV: 4
         };
         this.opAtual = this.op.NOP;
+    }
+
     
     mostraVisor() {
         return this.nrVisor;
     }
+
+ 
     recebeDigito(dig) {
         if (this.estadoErro) return;
         if (dig.length != 1) return;
@@ -39,6 +44,8 @@ class Calculadora {
             this.nrVisor += dig;
         }
     }
+
+
     defineOperacao(op) {
         if (this.estadoErro) return;
         switch (op) {
@@ -58,6 +65,112 @@ class Calculadora {
         this.memTemp = this.nrVisor;
     }
 
+    
+    igual() {
+        if (this.estadoErro) return;
+        if (this.opAtual == this.op.NOP) return;
+        let num1 = parseFloat(this.memTemp);
+        let num2 = parseFloat(this.nrVisor);
+        let resultado = 0;
+        switch (this.opAtual) {
+            case this.op.SUM:
+                resultado = num1 + num2;
+                break;
+            case this.op.SUB:
+                resultado = num1 - num2;
+                break;
+            case this.op.MULT:
+                resultado = num1 * num2;
+                break;
+            case this.op.DIV:
+                if (num2 == 0) {
+                    this.estadoErro = true;
+                    this.nrVisor = 'ERRO!';
+                    return;
+                }
+                resultado = num1 / num2;
+                break;
+        }
+        this.opAtual = this.op.NOP;
+        this.ptDecimal = false;
+        this.memTemp = '';
+        this.iniciouSegundo = false;
+        this.nrVisor = String(resultado).slice(0, 10);
+    }
 
+  
+    teclaC() {
+        this.nrVisor = '0';
+        this.ptDecimal = false;
+        this.iniciouSegundo = false;
+        this.opAtual = this.op.NOP;
+        this.memTemp = '';
+        this.estadoErro = false;
+    }
+
+  
+    teclaMmais() {
+        if (this.estadoErro) return;
+        this.memoria += parseFloat(this.nrVisor);
+    }
 
     
+    teclaMmenos() {
+        if (this.estadoErro) return;
+        this.memoria -= parseFloat(this.nrVisor);
+    }
+
+   
+    teclaRM() {
+        if (this.estadoErro) return;
+        this.nrVisor = String(this.memoria);
+    }
+
+    
+    teclaCLM() {
+        if (this.estadoErro) return;
+        this.memoria = 0;
+    }
+
+ 
+raizQuadrada(){
+if(this.estadoErro)return;
+let numero = parseFloat(this.nrVisor);
+if (numero < 0) {
+    this.estadoErro = true;
+    this.nrVisor = 'ERRO!';
+    return;
+    }
+    let raiz = Math.sqrt(numero);
+    this.nrVisor = String(raiz).slice(0, 10);
+
+}
+
+inverso() {
+    if (this.estadoErro) return;
+    let numero = parseFloat(this.nrVisor);
+    if (numero === 0) {
+        this.estadoErro = true;
+        this.nrVisor = 'ERRO!';
+        return;
+    }
+    let inverso = 1 / numero;
+    this.nrVisor = String(inverso).slice(0, 10);
+}
+
+   
+
+ 
+    calcularPorcentagem() {
+        if (this.estadoErro) return;
+        let numero = parseFloat(this.nrVisor);
+        let porcentagem = numero / 100;
+        this.nrVisor = String(porcentagem).slice(0, 10);
+    }
+
+    
+    alternarSinal() {
+        if (this.estadoErro) return;
+        this.nrVisor = -parseFloat(this.nrVisor);
+    }
+}
